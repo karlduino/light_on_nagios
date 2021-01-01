b360d168afd993facafac06820947cda994f99b3
Karlduino <karlduino@gmail.com>
2021-01-01 13:35:31 -0600

    starting work on function to parse the file

--- light_on_nagios.py
+++ light_on_nagios.py
@@ -8,6 +8,10 @@ nagios_dir = '/usr/local/nagios/var'
 nagios_file = 'status.dat'
 local_dir = '/home/pi/nagios_data'
 
+hosts = ['main_router', 'tv_router', 'unfinished_router']
+
+local_file = os.path.join(local_dir, nagios_file)
+
 
 def copy_nagios_file(server, server_dir, file, local_dir):
     local_path = os.path.join(local_dir, file)
@@ -30,6 +34,18 @@ def copy_nagios_file(server, server_dir, file, local_dir):
     # scp worked and file now exists
     return (scp_result==0 and os.path.isfile(local_path))
 
+def parse_nagios_file(nagios_file, host_names):
+    host_up = [False for name in host_names] 
+
+    if not os.path.isfile(nagios_file): # if no file, assume they're all down
+        return host_up
+
+    return host_up
+
+
+
 # run the stuff
 scp_result = copy_nagios_file(nagios_server, nagios_dir, nagios_file, local_dir)
+host_status = parse_nagios_file(local_file, hosts)
 print(scp_result)
+print(host_status)
