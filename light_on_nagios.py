86f9e41be9a4b5fc36963fc45f12f4806c17c52c
Karlduino <karlduino@gmail.com>
2021-01-01 14:32:20 -0600

    Add led light aspect

--- light_on_nagios.py
+++ light_on_nagios.py
@@ -1,14 +1,36 @@
 #!/usr/bin/env python3
 
+from gpiozero import LED
 import os
 import subprocess
+from time import sleep
+from math import ceil
 
+# wait time between checks
+wait_time = 60     # in sec
+startup_wait = 0.2 # in sec
+blink_time=0.1     # on/off time for blinking; 0 for no blinking
+max_blink=30       # max time blinking (secs)
+
+# server/file info
 nagios_server = 'nagiospi.local'
 nagios_dir = '/usr/local/nagios/var'
 nagios_file = 'status.dat'
 local_dir = '/home/pi/nagios_data'
 
+# hosts to monitor
 hosts = ['main_router', 'tv_router', 'unfinished_router']
+
+# LED pins
+green1 = 25    # (pin 22 on old raspberry pi with 13x2 layout)
+red1   = 24    # (pin 18)
+
+green2 = 23    # (pin 16)
+red2   = 22    # (pin 15)
+
+green3 = 27    # (pin 13)
+red3   =  4    # (pin 7)
+
 
 local_file = os.path.join(local_dir, nagios_file)
 
@@ -67,8 +89,39 @@ def parse_nagios_file(nagios_file, host_names):
     return [host_status[host]=='0' for host in host_names]
 
 
-# run the stuff
-scp_result = copy_nagios_file(nagios_server, nagios_dir, nagios_file, local_dir)
-host_status = parse_nagios_file(local_file, hosts)
-print(scp_result)
-print(host_status)
+def light_if_up(host_up, green_led, red_led):
+    if host_up:
+        green_led.on()
+        red_led.off()
+    else:
+        green_led.off()
+        red_led.on()
+
+def startup(leds, wait):
+    for led in leds:
+        led.on()
+        sleep(wait)
+    sleep(wait)
+    for led in leds:
+        led.off()
+        sleep(wait)
+
+
+led_green = [LED(green1), LED(green2), LED(green3)]
+led_red   = [LED(red1), LED(red2), LED(red3)]
+
+# now run everything
+
+startup(led_green + led_red, startup_wait)
+
+while True:
+    if blink_time > 0: 
+        led_green[0].blink(blink_time, blink_time, ceil(max_blink/blink_time), True)
+
+    scp_result = copy_nagios_file(nagios_server, nagios_dir, nagios_file, local_dir)
+    host_status = parse_nagios_file(local_file, hosts)
+    
+    for i in range(0,3):
+        light_if_up(host_status[i], led_green[i], led_red[i])
+
+    sleep(wait_time)
